6a205da4eb3146a309ed76917076b30ee9818f3a
Edwin <deronviledwin@gmail.com>
2019-12-13 14:20:40 -0500

    added more styles

--- starwars/src/StarNames.js
+++ starwars/src/StarNames.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { Card, CardImg, CardTitle } from "reactstrap";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+
+background-color: red;
+`;
 const Appcontent = props =>{
     console.log(props.birth_year);
     return (
         <div className='star-container' key={props.x}>
-            
-            <Card>
-                <CardTitle>
-                    <h1>{props.name}</h1>
-                </CardTitle>
-                <p>{props.gender}</p>
-                <p>{props.birth_year}</p>
-            </Card>
+            <Wrapper>
+                <Card>
+                    <CardTitle>
+                        <h1>{props.name}</h1>
+                    </CardTitle>
+                    <p>{props.gender}</p>
+                    <p>{props.birth_year}</p>
+                </Card>
+            </Wrapper>
             <div className="hero">
                 
             </div>
